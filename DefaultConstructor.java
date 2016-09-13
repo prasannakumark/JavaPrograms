472aefba25e981e2569457f4d66e7614b17e73fb
Leelavathi <leelavathi.pg114@gmail.com>
2016-09-13 18:28:12 +0530

    Default Constructor Example Added

---
+++ DefaultConstructor.java
@@ -0,0 +1,13 @@
+public class DefaultConstructor{
+
+DefaultConstructor(){
+System.out.println("Default Constructor Created");
+}
+
+
+public static void main(String args[]){
+
+DefaultConstructor def = new DefaultConstructor();
+}
+}
+
