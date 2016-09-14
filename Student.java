759e0ed8f6a8e96f74228f0fd3b703b532daf339
Leelavathi <leelavathi.pg114@gmail.com>
2016-09-14 11:59:13 +0530

    Constructor Overloading Added

---
+++ Student.java
@@ -0,0 +1,31 @@
+public class Student{
+
+int id;
+String name;
+int age;
+
+ Student(int i, String n){
+
+id = i;
+name = n;
+}
+
+ Student(int i, String n, int a){
+
+id = i;
+name = n;
+age = a;
+}
+void display(){
+
+System.out.println(id+" "+name+" "+age);
+}
+
+public static void main(String args[]){ 
+
+Student stud = new Student(111, "Karan",25);
+
+stud.display();
+}
+
+}
