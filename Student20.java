1c2f66a80bd5d0e322a0058df180115889f9a7c5
Leelavathi <leelavathi.pg114@gmail.com>
2016-09-22 18:54:31 +0530

    super keyword example added that invokes parent class method

---
+++ Student20.java
@@ -0,0 +1,17 @@
+class Person{  
+void message(){System.out.println("welcome");}  
+}  
+  
+class Student20 extends Person{  
+void message(){System.out.println("welcome to java");}  
+  
+void display(){  
+message();//will invoke current class message() method  
+super.message();//will invoke parent class message() method  
+}  
+  
+public static void main(String args[]){  
+Student20 s=new Student20();  
+s.display();  
+}  
+}  
